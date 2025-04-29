3ae77bfe29de9d4d64965e06637ab540369aeaef
Davron Chetin <nabijonovdavronbek619@gmail.com>
2025-04-29 11:05:09 +0500

    shoe case slider

--- src/page_parts/show_case.tsx
+++ src/page_parts/show_case.tsx
@@ -7,6 +7,7 @@ import Btn from "../components/btn";
 // images
 import show_Case from "../assets/Vector 14.png";
 import show_Case_hover from "../assets/Vector 15.png";
+import circle from '../assets/Frame 36.png'
 
 export default function Show_case() {
   // aos animation
@@ -18,12 +19,12 @@ export default function Show_case() {
   }, []);
 
   return (
-    <div data-aos="fade-up">
+    <div data-aos="fade-up-right">
 
       <div className="show_case my-[120px]">
-        <div className="container relative ">
+        <div className="container relative max-lg:flex max-lg:items-center max-lg:flex-col max-lg:justify-center max-lg:gap-[100px] ">
 
-        <div className="grp_absolute">
+        <div className="grp_absolute lg:inline hidden ">
               <img
                 src={show_Case_hover}
                 className=" absolute top-[22px] left-[500px] w-[900px] z-[-1] "
@@ -38,7 +39,7 @@ export default function Show_case() {
             />
           </div>
 
-          <div className="grp_left flex flex-col gap-[30px] mt-[50px] ">
+          <div className="grp_left flex flex-col gap-[30px] mt-[50px] max-lg:w-full max-lg:justify-center max-lg:items-center max-lg:gap-[30px] ">
             <div className="with_span flex items-center gap-[10px] text-white text-[63px] font-[700]   ">
               <TextTranslater txt="head-your" />
               <TextTranslater txt="head-growth" txt_styles="text-[#4CCED0]" />
@@ -49,9 +50,9 @@ export default function Show_case() {
             />
             <TextTranslater
               txt="head-txt"
-              txt_styles=" text-[22px] text-white w-[500px] mt-[30px] "
+              txt_styles=" text-[22px] text-white w-[500px] mt-[30px] lg:text-left text-center "
             />
-            <div className="btn_grp flex gap-[15px] mt-[30px] text-white ">
+            <div className="btn_grp flex gap-[15px] mt-[30px] text-white  ">
               <Btn
                 txt="started"
                 btn_styles="w-[180px] h-[50px] rounded-[18px] p-[10px] bg-[#4CCED0] hover:bg-[#848484] text-[18px] text-white "
@@ -62,7 +63,8 @@ export default function Show_case() {
               />
             </div>
           </div>
-
+ 
+          <img src={circle} className="  lg:absolute  max-lg:w-[200px]  bottom-[80px] right-[600px] " loading="lazy" alt='circle image' />
           
 
         </div>
---
+++ src/sliders/showCase_swiper.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "../App.css";
+// Slider.jsx
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation, Autoplay } from "swiper/modules";
+
+// icon
+import { FaStar } from "react-icons/fa6";
+
+export default function ShowCase_swiper() {
+  return (
+    <div>
+      <div className="container">
+        <Swiper
+          modules={[Navigation, Autoplay]}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          navigation
+          spaceBetween={30}
+          slidesPerView={1}
+          className=" showCase-swiper w-full"
+        >
+          <SwiperSlide>
+            <p className=" text-white flex items-center gap-[30px] text-[20px] ">
+              Google <FaStar />
+            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <p className=" text-white flex items-center gap-[30px] text-[20px] ">
+              Twitter <FaStar />
+            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <p className=" text-white flex items-center gap-[30px] text-[20px] ">
+              Facebook <FaStar />
+            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <p className=" text-white flex items-center gap-[30px] text-[20px] ">
+              Instagram <FaStar />
+            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <p className=" text-white flex items-center gap-[30px] text-[20px] ">
+              Dribble <FaStar />
+            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <p className=" text-white flex items-center gap-[30px] text-[20px] ">
+              Behance <FaStar />
+            </p>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
+}
