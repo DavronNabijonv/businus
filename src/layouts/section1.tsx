925d14d89e4d440fdafbda56de564b0169381bb1
Davron Chetin <nabijonovdavronbek619@gmail.com>
2025-04-29 13:58:37 +0500

    show about part

--- src/layouts/section1.tsx
+++ src/layouts/section1.tsx
@@ -1,10 +1,12 @@
 
+import Main_about from '../page_parts/main_about'
 import Show_case from '../page_parts/show_case'
 
 export default function Section1() {
   return (
     <div>
       <Show_case/>
+      <Main_about/>
     </div>
   )
 }
---
+++ src/page_parts/main_about.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Main_about() {
+  return (
+    <div>Main_about</div>
+  )
+}
